c564e4b970017b8f601d6cfe98213d832915787d
sahanaSu <sahanags2@gmail.com>
2021-08-11 19:32:47 +0530

    titile made bold

--- task3_3.js
+++ task3_3.js
@@ -1,3 +1,5 @@
 var header = document.getElementById('main-header');
 console.log(header);
 header.style.borderBottom = 'solid 3px black';
+var titles = document.querySelectorAll('.title');
+titles[0].style.fontweight = 'bold';
