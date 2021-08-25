40746c165f5f9474fc1212ed1ddb4fcbecd91dab
sahanaSu <88713222+sahanaSu@users.noreply.github.com>
2021-08-25 15:17:10 +0530

    Create currying_bind.js
    currying concept using bind

---
+++ currying_bind.js
@@ -0,0 +1,15 @@
+let multiply = function(x,y){
+	return x*y;
+}
+
+let multiplyByTwo = multiply.bind(this, 2);
+console.log(multiplyByTwo(3));
+
+let multiplyByTwo1 = multiply.bind(this, 2 ,5);
+console.log(multiplyByTwo1(3));
+
+let multiplyByTwo2 = multiply.bind(this,);
+console.log(multiplyByTwo2(3,5));
+
+let multiplyByThree = multiply.bind(this, 3);
+console.log(multiplyByThree(3));
