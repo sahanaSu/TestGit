9d3d2abdb70dbbc8c400725efb8e1baf179e0ad3
sahanaSu <88713222+sahanaSu@users.noreply.github.com>
2021-08-25 15:03:32 +0530

    Update using_call.js
    understanding call apply and commit

--- using_call.js
+++ using_call.js
@@ -1,4 +1,4 @@
-var obj = {num:2};
+1.var obj = {num:2};
 
 	var add = function(){
 
@@ -7,3 +7,43 @@ var obj = {num:2};
 	}
 
 	console.log(add.call(obj));
+
+2.var obj = {num:2};
+
+	var add = function(a,b,c){
+
+		return this.num+a+b+c;
+
+	}
+
+	var arr=[1,2,3];
+
+	console.log(add.apply(obj,arr));
+
+3.var obj = {num:2};
+
+	var add = function(a,b,c){
+
+		return this.num+a+b+c;
+
+	}
+
+
+
+	var bound = add.bind(obj);
+
+	console.log(bound(1,2,3));
+
+4.var student = {age:20};
+
+	var add = function(){
+
+		return this.age;
+
+	}
+
+
+
+	var bound = add.bind(this);
+
+	console.log(bound());
